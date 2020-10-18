a2ba1adaa75266491a936f54ace7afa05e99e021
Mitchell Gust s3782095 <s3782095@student.rmit.edu.au>
2020-10-18 20:49:52 +1100

    Enable Extra Broken Row

--- src/Types.h
+++ src/Types.h
@@ -19,7 +19,7 @@
 #define MOSAIC_BOARD_SIZE (MOSAIC_DIM * MOSAIC_DIM)
 #define NUM_OF_FACTORIES 5
 #define FACTORY_SIZE 4
-#define BROKEN_ROW_SIZE 7
+#define BROKEN_ROW_SIZE 8
 #define MOSAIC_BOARD_COLOR "byruloobyrullobyruulobyrrulobyyrulob"
 #define FIRST_FACTORY 0
 #define LAST_FACTORY 5
